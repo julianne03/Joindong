f149c75210531e4080ce4f97df6b9cc56b9b2d9f
julianne03 <s2019w33@e-mirim.hs.kr>
2021-06-20 02:59:13 +0900

    jd/urls club_list

--- jd/urls.py
+++ jd/urls.py
@@ -5,5 +5,6 @@ from jd import views
 app_name = 'jd'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('/club_list', views.club_list, name='club_list'),
 ]
