738bc5c57e419bc6617be1c35e746f42ea7a1501
julianne03 <s2019w33@e-mirim.hs.kr>
2021-06-19 19:46:31 +0900

    jd/models Club, Message

--- jd/models.py
+++ jd/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Club(models.Model):
+    title = models.CharField(max_length=30)
+    main_poster = models.ImageField()
+    description = models.TextField(max_length=100)
+    activities = models.TextField()
+    good_things = models.TextField()
+    etc_things = models.TextField()
+    deadline = models.DateTimeField()
+    head_num = models.IntegerField(default=1)
+
+
+class Message(models.Model):
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    plan_message = models.TextField()
+    pass_message = models.TextField()
+    non_pass_message = models.TextField()
+
