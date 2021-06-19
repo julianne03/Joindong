63e776a18b8989a919e62fcd582bfdcd06b07960
julianne03 <s2019w33@e-mirim.hs.kr>
2021-06-20 02:58:41 +0900

    jd/views club_list

--- jd/views.py
+++ jd/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from jd.models import Club
+
 
 def index(request):
     return render(request, 'jd/index.html')
+
+
+def club_list(request):
+    clubs = Club.objects.order_by('deadline')
+    context = {'list': clubs}
+    return render(request, 'jd/club_list.html', context)
