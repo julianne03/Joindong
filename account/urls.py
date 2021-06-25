117214d495ba91d6d42bc3113b414c588ac46f5a
julianne03 <s2019w33@e-mirim.hs.kr>
2021-06-26 02:21:01 +0900

    account/urls signup

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,5 @@ app_name = 'account'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('signup/', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
 ]
