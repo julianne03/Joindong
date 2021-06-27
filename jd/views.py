843d168ae94133b3a15ad258387b41858fa7a72f
julianne03 <s2019w33@e-mirim.hs.kr>
2021-06-27 19:05:58 +0900

    jd/views create_club

--- jd/views.py
+++ jd/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
+from jd.forms import ClubForm
 from jd.models import Club
 
 
@@ -11,3 +13,18 @@ def club_list(request):
     clubs = Club.objects.order_by('deadline')
     context = {'list': clubs}
     return render(request, 'jd/club_list.html', context)
+
+
+@login_required(login_url='account:login')
+def create_club(request):
+    if request.method == 'POST':
+        form = ClubForm(request.POST, request.FILES)
+        if form.is_valid():
+            club = form.save(commit=False)
+            club.creator = request.user
+            club.main_poster = request.FILES['main_poster_image']
+            club.save()
+            return redirect('jd:club_list')
+    else:
+        form = ClubForm()
+    return render(request, 'jd/create_club.html', {'form': form})
