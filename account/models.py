05576b204d449fa17803ab4c59daf37754796eea
julianne03 <s2019w33@e-mirim.hs.kr>
2021-06-27 19:05:37 +0900

    account/models Profile - alter club

--- account/models.py
+++ account/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from jd.models import Club
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,7 +10,7 @@ class Profile(models.Model):
     nickname = models.CharField(max_length=20)
     image = models.ImageField(blank=True, upload_to="profile_image/")
     phone_num = models.CharField(max_length=20)
-    club_name = models.TextField(blank=True)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE, null=True, blank=True)
     is_club_staff = models.BooleanField(default=False)
 
     def __str__(self):
