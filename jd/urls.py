80ff1b73fb12d913bb6367d23f3a00cd0da538aa
julianne03 <s2019w33@e-mirim.hs.kr>
2021-06-27 19:04:00 +0900

    jd/urls club/create/

--- jd/urls.py
+++ jd/urls.py
@@ -7,4 +7,5 @@ app_name = 'jd'
 urlpatterns = [
     path('', views.index, name='index'),
     path('club_list/', views.club_list, name='club_list'),
+    path('club/create/', views.create_club, name='club_create'),
 ]
