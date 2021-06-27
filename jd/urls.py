718a29b642dc8b498d6b06c7bdf6b4f76dfe46d1
julianne03 <s2019w33@e-mirim.hs.kr>
2021-06-27 23:34:58 +0900

    jd/urls detail club

--- jd/urls.py
+++ jd/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('club_list/', views.club_list, name='club_list'),
     path('club/create/', views.create_club, name='club_create'),
+    path('club/detail/<str:club_title>/', views.detail_club, name='club_detail'),
 ]
