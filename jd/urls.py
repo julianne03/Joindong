0fe8c48d0762b617010b29d90877941bb7c605db
julianne03 <s2019w33@e-mirim.hs.kr>
2021-06-30 00:01:31 +0900

    jd/urls mypage

--- jd/urls.py
+++ jd/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('club/modify/<str:club_title>/', views.update_club, name='club_update'),
     path('club/message/modify/<str:club_title>/', views.update_club_message, name='club_message_update'),
     path('club/<str:user_name>/', views.my_club, name='my_club'),
+    path('mypage/<str:user_name>/', views.my_page, name='my_page'),
     path('club/apply/<str:club_title>/', views.apply_club, name='club_apply'),
     path('club/cancel/apply/<str:club_title>/', views.cancel_apply_club, name='club_cancel_apply'),
 ]
